4d28db461d4965109b3ae307819c6d09d58269f0
Roburt96 <roburtsulev96@gmail.com>
2023-04-15 19:18:47 +0300

    done

---
+++ C# Basics/04. For Loop/Exercise/Exercise/Histogram/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            histogram();
+        }
+
+        static void histogram()
+        {
+            Dictionary<string, int> data = new Dictionary<string, int>()
+            {
+                {"p1", 0},
+                {"p2", 0},
+                {"p3", 0},
+                {"p4", 0},
+                {"p5", 0}
+            };
+            
+
+            var range = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < range; i++)
+            {
+                var currentNumber = Convert.ToInt32(Console.ReadLine());
+                if (0 <= currentNumber && currentNumber < 200)
+                {
+                    data["p1"] += 1;
+                }
+                else if (200 <= currentNumber && currentNumber < 399)
+                {
+                    data["p2"] += 1;
+                }
+                
+                else if (400 <= currentNumber && currentNumber < 599)
+                {
+                    data["p3"] += 1;
+                }
+                
+                else if (600 <= currentNumber && currentNumber < 799)
+                {
+                    data["p4"] += 1;
+                }
+                
+                else if (currentNumber >= 800)
+                {
+                    data["p5"] += 1;
+                }
+  
+            }
+            
+            foreach (var item in data)
+            {
+                
+                double percent = ((double)item.Value / range) * 100;
+                Console.WriteLine($"{percent:F2}%");
+            }
+        }
+    }
+}
---
+++ C# Basics/04. For Loop/Exercise/Exercise/Numbers Ending in 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Numbers_Ending_in_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            endingNumbers();
+        }
+
+        static void endingNumbers()
+        {
+            for (int i = 7; i < 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
