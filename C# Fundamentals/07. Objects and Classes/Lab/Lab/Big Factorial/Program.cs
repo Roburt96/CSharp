3af62f593fdce877ecb5f08703e8f559a97368b0
Roburt96 <roburtsulev96@gmail.com>
2023-04-23 16:52:10 +0300

    done

---
+++ C# Fundamentals/07. Objects and Classes/Lab/Lab/Big Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+
+namespace Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            BigInteger result = Factorial(n);
+            Console.WriteLine(result);
+        }
+
+        static BigInteger Factorial(BigInteger number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+    }
+}
---
+++ C# Fundamentals/07. Objects and Classes/Lab/Lab/Students/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+
+                string[] cmds = input.Split(' ');
+                Student student = new Student();
+                student.firstName = cmds[0];
+                student.lastName = cmds[1];
+                student.age = Convert.ToInt32(cmds[2]);
+                student.homeTown = cmds[3];
+                
+                students.Add(student);
+            }
+            
+            string city = Console.ReadLine();
+            foreach (var student in students)
+            {
+                if (student.homeTown == city)
+                {
+                    Console.WriteLine($"{student.firstName} {student.lastName} is {student.age} years old.");
+                }
+            }
+            
+        }
+    }
+
+    class Student
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public string homeTown { get; set; }
+        
+    }
+}
