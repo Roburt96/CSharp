b6adbc38d1e8b411cc38c4cbd7491eda0b5c8c43
Roburt96 <roburtsulev96@gmail.com>
2024-05-26 11:37:48 +0300

    exam 13122023

---
+++ C# Advanced/Exams/13122023/2/TheGambler/Program.cs
@@ -0,0 +1,112 @@
+﻿namespace TheGambler
+{
+    class Gambler
+    {
+        static void Main()
+        {
+            var input = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[input][];
+            var pRow = 0; var pCol = 0; var amount = 100;
+
+            for (int i = 0; i < input; i++)
+            {
+                matrix[i] = Console.ReadLine().ToCharArray();
+            }
+            for (int i = 0; i < input; i++)
+            {
+                for (int j = 0; j < input; j++)
+                {
+                    if (matrix[i][j] == 'G')
+                    {
+                        pRow = i;
+                        pCol = j;
+                        matrix[pRow][pCol] = '-';
+                    }
+                }
+            }
+
+            
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "end") { break; }
+
+                if (command == "left")
+                {
+                    pCol--;
+                }
+                else if (command == "right")
+                {
+                    pCol++;
+                }
+                else if (command == "up")
+                {
+                    pRow--;
+                }
+                else if (command == "down")
+                {
+                    pRow++;
+                }
+
+                if (amount <= 0)
+                {
+                    break;
+                }
+                if (amount >= 100000)
+                {
+                    break;
+                }
+
+                if (matrix[pRow][pCol] == 'W')
+                {
+                    amount += 100;
+                    matrix[pRow][pCol] = '-';
+                }
+                else if (matrix[pRow][pCol] == 'P')
+                {
+                    amount -= 200;
+                    matrix[pRow][pCol] = '-';
+                }
+                else if (matrix[pRow][pCol] == 'J')
+                {
+                    amount += 100000;
+                    matrix[pRow][pCol] = '-';
+                    Console.WriteLine("You win the Jackpot!");
+                }
+                if (amount <= 0)
+                {
+                    break;
+                }
+                if (amount >= 100000)
+                {
+                    break;
+                }
+
+            }
+             matrix[pRow][pCol] = 'G';
+            if (amount > 0)
+            {
+                Console.WriteLine($"End of the game. Total amount: {amount}$");
+                foreach(var row in  matrix)
+                {
+                    Console.WriteLine(new string(row));
+                }
+            }
+            else { Console.WriteLine("Game Over! You lost everythink!"); }
+
+
+        }
+
+        
+
+
+
+
+
+        static bool Coordinate(int row, int col, int rows, int cols)
+        {
+            return row < 0 && row > rows && col < 0 && col > cols;
+        }
+    }
+}
---
+++ C# Advanced/Exams/13122023/3/GroceriesManagement/GroceriesStore.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+namespace GroceriesManagement
+{
+    public class GroceriesStore
+    {
+        public int Capacity { get; set; }
+        public double Turnover { get; set; }
+        public List<Product> Stall {  get; set; }
+        public double Total { get; set; }
+
+        public GroceriesStore(int capacity)
+        {
+            Capacity = capacity;
+            Turnover = 0;
+            Stall = new List<Product>();
+            Total = 0;
+        }
+
+        public void AddProduct(Product product)
+        {
+            if (Stall.Count >= Capacity)
+            {
+                return;
+            }
+            if(Stall.Any(x => x.Name == product.Name))
+            {
+                return;
+            }
+            Stall.Add(product);
+        }
+        public bool RemoveProduct(string name)
+        {
+            var product = Stall.FirstOrDefault(x => x.Name == name);
+            if (product != null)
+            {
+                Stall.Remove(product);
+                return true;
+            }
+            return false;
+        }
+
+        public string SellProduct(string name, double quantity)
+        {
+            var product = Stall.FirstOrDefault(x=>x.Name == name);
+            if (product != null)
+            {
+                Turnover = product.Price * quantity;
+                Total += Turnover;
+                return $"{product.Name} - {Turnover:F2}$";
+            }
+            return "Product not found";
+        }
+
+        public string GetMostExpensive()
+        {
+            var product = Stall.OrderByDescending(x => x.Price).FirstOrDefault();
+            return product.ToString();
+        }
+        
+        public string CashReport()
+        {
+            return $"Total Turnover: {Total:F2}$";
+        }
+
+        public string PriceList()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Groceries Price List:");
+            foreach (var item in Stall)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
