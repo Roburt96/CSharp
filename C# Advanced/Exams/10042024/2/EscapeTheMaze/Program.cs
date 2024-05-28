dbc7e8e743c6f6f48ea727a9e8c4eb4d2d48cb1b
Roburt96 <roburtsulev96@gmail.com>
2024-05-28 16:24:01 +0300

    exam 10042024

---
+++ C# Advanced/Exams/10042024/2/EscapeTheMaze/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        char[,] field = new char[N, N];
+
+        int playerRow = -1;
+        int playerCol = -1;
+        for (int row = 0; row < N; row++)
+        {
+            string line = Console.ReadLine();
+            for (int col = 0; col < N; col++)
+            {
+                field[row, col] = line[col];
+                if (line[col] == 'P')
+                {
+                    playerRow = row;
+                    playerCol = col;
+                }
+            }
+        }
+
+        int playerHealth = 100;
+        bool isPlayerAlive = true;
+        string command;
+        while ((command = Console.ReadLine()) != null && command != "")
+        {
+            int nextRow = playerRow;
+            int nextCol = playerCol;
+
+            if (command == "up")
+            {
+                nextRow--;
+            }
+            else if (command == "down")
+            {
+                nextRow++;
+            }
+            else if (command == "left")
+            {
+                nextCol--;
+            }
+            else if (command == "right")
+            {
+                nextCol++;
+            }
+
+            if (!IsValidPosition(nextRow, nextCol, N))
+            {
+                continue;
+            }
+
+            if (field[nextRow, nextCol] == 'M')
+            {
+                playerHealth -= 40;
+                if (playerHealth <= 0)
+                {
+                    playerHealth = 0;
+                    isPlayerAlive = false;
+                    field[nextRow, nextCol] = 'P';
+                    break;
+                }
+            }
+            else if (field[nextRow, nextCol] == 'H')
+            {
+                playerHealth += 15;
+                if (playerHealth > 100)
+                {
+                    playerHealth = 100;
+                }
+            }
+            else if (field[nextRow, nextCol] == 'X')
+            {
+                isPlayerAlive = true;
+                field[nextRow, nextCol] = 'P';
+                break;
+            }
+            field[playerRow, playerCol] = '-';
+            playerRow = nextRow;
+            playerCol = nextCol;
+        }
+        Console.WriteLine(isPlayerAlive ? "Player escaped the maze. Danger passed!" : "Player is dead. Maze over!");
+        Console.WriteLine($"Player's health: {playerHealth} units");
+        for (int row = 0; row < N; row++)
+        {
+            for (int col = 0; col < N; col++)
+            {
+                Console.Write(field[row, col]);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static bool IsValidPosition(int row, int col, int N)
+    {
+        return row >= 0 && row < N && col >= 0 && col < N;
+    }
+}
---
+++ C# Advanced/Exams/10042024/3/DataCenter/Rack.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace DataCenter
+{
+    public class Rack
+    {
+        public int Slots { get; set; }
+        public List<Server> Servers { get; set; }
+        public int GetCount { get { return Servers.Count; } }
+
+        public Rack(int slots)
+        {
+            Slots = slots;
+            Servers = new List<Server>();
+        }
+        public void AddServer(Server server)
+        {
+            var currServer = Servers.FirstOrDefault(x => x.SerialNumber == server.SerialNumber);
+            if (currServer == null)
+            {
+                if (Servers.Count < Slots)
+                {
+                    Servers.Add(server);
+                }
+            }
+        }
+        
+        public bool RemoveServer(string serialnumber)
+        {
+            var server = Servers.FirstOrDefault(y => y.SerialNumber == serialnumber);
+            if (server != null)
+            {
+                Servers.Remove(server);
+                return true;
+            }
+            return false;
+        }
+
+        public string GetHighestPowerUsage()
+        {
+            var highestPowerServer = Servers.OrderByDescending(x => x.PowerUsage).FirstOrDefault();
+            return highestPowerServer != null ? highestPowerServer.ToString() : "No servers available";
+        }
+
+        public int GetTotalCapacity()
+        {
+            return Servers.Sum(x => x.Capacity);
+        }
+
+        public string DeviceManager()
+        {
+            var serverList = Servers.Select(x => x.ToString()).ToList();
+            return $"{Servers.Count} servers operating:\n{string.Join("\n", serverList)}";
+        }
+    }
+}
